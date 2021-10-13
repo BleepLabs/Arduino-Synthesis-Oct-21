b7c16baa3b003d5f9e3e066ae1f3fb3095af7b8a
Dr. Bleep <drbleep@bleeplabs.com>
2021-10-13 18:56:27 -0500

    Create Class_2_-_blink_1.ino

---
+++ Class_code/Class_2/Class_2_-_blink_1/Class_2_-_blink_1.ino
@@ -0,0 +1,34 @@
+unsigned long current_time;
+unsigned long prev_time;
+int led_state;
+int rate1 = 200;
+
+void setup() {
+  pinMode(13, OUTPUT); //only for LEDs
+
+}
+
+void loop() {
+  current_time = millis();
+
+  rate1 = 1023 - analogRead(A10); //analogRead returns 0-1023 10b ADC
+
+  if (current_time - prev_time > rate1) {
+    prev_time = current_time;
+
+    Serial.println(rate1);
+    //led_state = !led_state;
+
+    if (led_state == 0) {
+      led_state = 1;
+    }
+    else {
+      led_state = 0;
+    }
+
+    digitalWrite(13, led_state);
+
+
+  }
+
+}
