30bbb37015174047a5dd936be798398d2a91dcd9
Dr. Bleep <drbleep@bleeplabs.com>
2021-10-13 19:31:57 -0500

    Update Bleep_Base_starting_point_1.ino

--- Class_code/Class_2/Bleep_Base_starting_point_1/Bleep_Base_starting_point_1.ino
+++ Class_code/Class_2/Bleep_Base_starting_point_1/Bleep_Base_starting_point_1.ino
@@ -1,7 +1,6 @@
 
 #include "bleep_base.h" //Contains functions we'll need when using the bleep base
 
-
 unsigned long current_time;
 unsigned long prev_time[8]; //an array of 8 variables all named "prev_time"
 float hue;
@@ -13,29 +12,42 @@ void setup() {
 } //setup is over
 
 
-
 void loop() {
   current_time = millis();
 
-  for (int i = 0; i < 8; i++)  {
+  for (int i = 0; i < 8; i = i + 1)  {
     buttons[i].update();
   }
 
-  if (buttons[0].fell()) {
-    Serial.println("button 0 fell");
+  for (int i = 0; i < 8; i = i + 1)  {
+    if (buttons[i].fell()) {
+      //Serial.println("button 0 fell");
+      Serial.print(i);
+      Serial.println(" fell!");
+    }
   }
+
   if (buttons[0].rose()) {
 
   }
-  if (buttons[0].read() == 0) {
+  if (buttons[0].read() == 0) { //0 is pressed,  1 is not pressed
 
   }
 
   hue = potRead(0); //0-1.0
+  //hue = 1.0-(analogRead(A10)/1023.0);
   bright = potRead(1); //0-1.0
 
   if (current_time - prev_time[0] > 33) { //33 miliseconds is about 30 Hz
     prev_time[0] = current_time;
+
+    //set_LED(led to change, hue,saturation,value aka brightness)
+    // led to change is 0-63
+    // all other are 0.0 to 1.0
+    // hue - 0 is red, then through the ROYGBIV to 1.0 as red again
+    // saturation - 0 is fully white, 1 is fully colored.
+    // value - 0 is off, 1 is the value set by max_brightness
+    // (it's not called brightness since, unlike in photoshop, we're going from black to fully lit up
 
     set_LED(0, hue, 1, bright);
     set_LED(1, hue + .6, 1, bright);
